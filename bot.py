7af7ac4f89ee8144938954b5834849f04afe22bd
Daniel Stojcevic <daniel.stojcevic@googlemail.com>
2011-05-30 16:54:37 +0200

    argument handling

--- bot.py
+++ bot.py
@@ -1,14 +1,53 @@
 import sys, time
 import socket
 import string
+import argparse
 
-def bot():
-	HOST="irc.freenode.net"
-	PORT=6667
-	NICK="Bot"
-	IDENT="bot"
-	REALNAME="IRC Bot"
+def cmd():
+	parser = argparse.ArgumentParser()
+
+# IRC
+	parser.add_argument('-s', action='store', dest='server',
+						help='Set IRC server')
+	parser.add_argument('-p', action='store', dest='port',
+						help='Set IRC port')
+	parser.add_argument('-n', action='store', dest='nick',
+						help='Set IRC nick')
+	parser.add_argument('-i', action='store', dest='ident',
+						help='Set IRC ident')
+	parser.add_argument('-r', action='store', dest='real',
+						help='Set IRC real name')
+# daemon
+	parser.add_argument('-D', action='store_false', default=True,
+						dest='deactivate_daemon',
+						help='Deactivate deamon')
 	
+	return parser.parse_args()
+
+def bot(args):
+# -s
+	HOST="irc.freenode.net"
+	if (args.server):
+		HOST=args.server
+# -p
+	PORT=6667
+	if (args.port):
+		PORT=args.port
+# -n
+	NICK="Bot"
+	if (args.nick):
+		NICK=args.nick
+# -i
+	IDENT="bot"
+	if (args.ident):
+		IDENT=args.ident
+# -r
+	REALNAME="IRC Bot"
+	if (args.real):
+		REALNAME=args.real
+	
+	print args
+	print HOST
 	#server connect 
 	IRCsocket=socket.socket()
 	IRCsocket.connect((HOST, PORT))
@@ -18,4 +57,5 @@ def bot():
 	IRCsocket.send("QUIT\r\n")
 	
 if __name__ == "__main__":
-	bot()
+	args = cmd()
+	bot(args)
